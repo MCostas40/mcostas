de0686bfe23f200a90d579267fc657388665546a
MCostas40 <123829087+MCostas40@users.noreply.github.com>
2023-01-28 14:50:14 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,7 @@
+
+document.getElementById("boton").onclick = function(){
+    console.log("Muchas Gracias por el Click!");
+}
+    //document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS"
+    //<p><a href="https://cammesaweb.cammesa.com/">Sitio web de CAMMESA S.A.</a></p>
+
